fd982872136bc2a6c65a796cf576b954b167054b
Cornell-GIT <otuonyechijioke@gmail.com>
2023-03-16 16:05:00 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program will ouput a specified line using printf followed by a new line
+ * Return: output zero (this indicates the end of the program)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
