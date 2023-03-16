5ffcb731005650ad1e18d300eb1ee1998efcbbce
Cornell-GIT <otuonyechijioke@gmail.com>
2023-03-16 15:15:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main: this program will output a line using the puts function
+ * Return: 0 (indicates the end of the program)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
