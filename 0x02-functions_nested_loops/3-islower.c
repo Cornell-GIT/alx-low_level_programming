807aa7d166df58239006b32f7a700ae395c0ef6d
Cornell-GIT <otuonyechijioke@gmail.com>
2023-03-21 13:55:23 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,13 @@
 #include "main.h"
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _islower - This program checks for lowercase characters
+ * @c: The character to be checked
+ * Return: 1 (success), if not 0
  */
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
