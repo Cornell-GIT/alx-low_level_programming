ad56e1fa94ee63a6773ab13dd7718df1d2fa2eae
Cornell-GIT <otuonyechijioke@gmail.com>
2023-03-21 15:19:47 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - this program adds two integers and returns the result
+ * @x: the first integer
+ * @y: the second integer
+ * Return: sum of the integers x and y
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
