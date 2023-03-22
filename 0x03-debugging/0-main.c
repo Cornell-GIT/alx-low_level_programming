2959e1eac8a3a74bd59fbf984a2eca77726fe935
Cornell-GIT <otuonyechijioke@gmail.com>
2023-03-22 12:59:32 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - This program tests that the function is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+this is the README file for the debugging tasks.. edit as required
