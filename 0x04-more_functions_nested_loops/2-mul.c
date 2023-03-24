efd2c89317eb7d6b12f3d75947300378ceb32086
Cornell-GIT <otuonyechijioke@gmail.com>
2023-03-23 18:39:35 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: product of the integers
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
