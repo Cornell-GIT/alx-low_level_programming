afb34f3b9fef269022ba6de534501c919a26b8b7
Cornell-GIT <otuonyechijioke@gmail.com>
2023-04-05 03:35:58 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
+int actual_prime(int n, int i);
+
 /**
  * is_prime_number - this function determines if an integer is a prime number
  * this function returns 1, if the integer is a prime number, else return 0
