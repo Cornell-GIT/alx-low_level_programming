4b68d6f0630cc08c2d4925ea1547cdb3b5925631
Cornell-GIT <otuonyechijioke@gmail.com>
2023-04-10 06:32:15 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - this function checks for digits in the string
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+
+int check_num(char *s)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < length(s))
+
+	{
+		if (!isdigit(s[count]))
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - this function prints the name of the program
+ * If no number is passed to the program, print 0, followed by a new line
+ * If one of the number contains symbols that are not digits, print Error,
+ * followed by a new line, and return 1
+ * Assume that numbers and the addition of all numbers can be stored in an int
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
